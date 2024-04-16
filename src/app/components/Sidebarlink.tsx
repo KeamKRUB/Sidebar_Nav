ae80ee5a8bc43e2a56f16dbacc42518945ba3c64
KeamKRUB <pawatchijaroen2004@gmail.com>
2024-04-17 03:14:05 +0700

    finish

--- src/app/components/Sidebarlink.tsx
+++ src/app/components/Sidebarlink.tsx
@@ -13,7 +13,8 @@ const SideLink = ({item,isCollapse}:LinkProp) => {
     const pathName = usePathname()
     console.log(pathName)
     return(
-<Link href={item.link} className={`${isCollapse && "justify-center"} items-center rounded p-1 inline-block text-lg flex rounded-[0.8rem] mb-[1rem] ${pathName!=item.link && "bg-[#d8d8d8]"}  ${pathName===item.link && "bg-green-300 text-black"}`}>
+<Link href={item.link} className={`${isCollapse && "justify-center"} items-center rounded p-1 inline-block text-lg flex rounded-[0.8rem] mb-[1rem] ${pathName!=item.link && "bg-[#66d6ff] text-blue-900"}  ${pathName===item.link && "bg-white text-black"}`}>
+
 
 <span className="sidebar_icon">
 <Image src={item.icon} alt="" width={24} height={24} />
